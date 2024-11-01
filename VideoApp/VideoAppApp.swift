05e65bdb9f30df00d5a7751376b1cbf9af959bfc
KobeBerckmans <kobe.berckmans@student.ehb.be>
2024-11-01 19:32:58 +0100

    Initial Commit

---
+++ VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Kobe Berckmans on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
